a380a3657779146ae931207b43845a94c1ec2e7f
SoloviovO <soloviov.o.m@gmail.com>
2023-01-20 17:29:08 +0200

    done first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-console.log('new');
+const bodyEl = document.querySelector('body');
+const changeColorBtn = document.querySelector('[data-start]');
+const stopChangeColorBtn = document.querySelector('[data-stop]');
+
+changeColorBtn.addEventListener('click', onChangeColorBtnClick);
+stopChangeColorBtn.addEventListener('click', onStopChangeColorBtnClick);
+
+let intervalId;
+
+function onChangeColorBtnClick() {
+  changeColorBtn.setAttribute('disabled', true);
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopChangeColorBtnClick() {
+  changeColorBtn.removeAttribute('disabled');
+  clearTimeout(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
